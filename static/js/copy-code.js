b89339ab22ac3188bb53f04e1f2fb3a6dc38aa5e
stevessr <steve-ssr@outlook.com>
2025-11-09 16:53:06 +0800

    feat: add copy button to code blocks

---
+++ static/js/copy-code.js
@@ -0,0 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((block) => {
+    var button = document.createElement('button');
+    button.className = 'copy-code-button';
+    button.type = 'button';
+    button.innerText = 'Copy';
+
+    var pre = block.parentNode;
+    if (pre.parentNode.classList.contains('highlight')) {
+      var highlight = pre.parentNode;
+      highlight.parentNode.insertBefore(button, highlight);
+    } else {
+      pre.parentNode.insertBefore(button, pre);
+    }
+
+    button.addEventListener('click', () => {
+      var code = block.innerText;
+      navigator.clipboard.writeText(code).then(() => {
+        button.innerText = 'Copied!';
+        setTimeout(() => {
+          button.innerText = 'Copy';
+        }, 2000);
+      }, (err) => {
+        console.error('Could not copy text: ', err);
+      });
+    });
+  });
+});
